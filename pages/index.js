4daef8e980edc23df69b5c6effa8b8331e686dda
Graham Billington <Grahamhbillington@gmail.com>
2022-06-24 18:52:04 -0400

    added search form page component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,34 @@
+import Layout from '../components/Layout';
+import SearchForm from '../components/SearchForm';
+
+export function getServerSideProps(ctx) {
+
+    // could fetch results on the server side here (would have to make an endpoint in /pages/api and fetch it)
+
+  return {
+    props: {
+        // results: [] // array of result objects from our API
+    }
+  };
+}
+
 export default function Home() {
-  return (
-    <div >
-    </div>
-  )
+
+    // could fetch results on the client side here
+
+    return (
+        <Layout title="Home" OGdescription="Free music for everyone" OGImagePath="some/path.png">
+            <div className="w-screen h-screen max-h-screen bg-green-300 flex justify-center items-center">
+
+                {/* search form */}
+                <SearchForm />
+
+                {/* results */}
+                <div className="flex flex-col items-center justify-start">
+
+                </div>
+
+            </div>
+        </Layout>
+    )
 }
